280d794d0f84adf31949be234e4a8921071955be
wrogistefan <lukasz.perek@outlook.com>
2026-01-12 18:54:29 +0100

    Add basic add_entry and list_entries to Vault

--- core/vault.py
+++ core/vault.py
@@ -2,5 +2,16 @@ class Vault:
     def __init__(self):
         self.entries = []
 
+    def add_entry(self, name: str, username: str, password: str):
+        entry = {
+            "name": name,
+            "username": username,
+            "password": password
+        }
+        self.entries.append(entry)
+
+    def list_entries(self):
+        return self.entries
+
     def remove_entry(self, name: str):
         self.entries = [e for e in self.entries if e["name"] != name]
