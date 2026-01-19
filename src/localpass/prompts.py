75edfb9253a1fa8b1bf0098d17f64e09e0d56ccd
GitHub Action <action@github.com>
2026-01-19 22:05:34 +0000

    Auto-format via CI

--- src/localpass/prompts.py
+++ src/localpass/prompts.py
@@ -3,7 +3,9 @@ import getpass
 import click
 
 
-def prompt_password_with_confirmation(initial_prompt: str, confirm_prompt: str = "Confirm password: ") -> str:
+def prompt_password_with_confirmation(
+    initial_prompt: str, confirm_prompt: str = "Confirm password: "
+) -> str:
     """Prompt for password and confirmation, retrying until they match."""
     while True:
         password = getpass.getpass(initial_prompt)
