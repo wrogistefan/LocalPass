c5845be0ce875e94f8b4c63fd4361f73211a03ec
GitHub Action <action@github.com>
2026-01-20 22:22:53 +0000

    Auto-format via CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
-import pytest
 from unittest.mock import patch
+
+import pytest
 
 
 @pytest.fixture(autouse=True)
 def mock_prompts():
-    with patch('click.prompt', return_value="testpass"), \
-         patch('getpass.getpass', return_value="testpass"):
+    with (
+        patch("click.prompt", return_value="testpass"),
+        patch("getpass.getpass", return_value="testpass"),
+    ):
         yield
